99b0e9ebb73063e1dc77e1956018e07ad184aae3
jumpjumpman <145435018+jumpjumpman028@users.noreply.github.com>
2024-12-09 18:14:02 +0800

    1.4.2 Version
    完成購物車數量同步

--- js/cart.js
+++ js/cart.js
@@ -1,3 +1,18 @@
+var cartNumber  ;
+function update(){
+    let cart = document.getElementById("cart-number");
+    cartNumber = parseInt(localStorage.getItem("cartnumber"));
+    cart.innerHTML = cartNumber;
+}
+function addToCart(){
+    update();
+    let buyNumber = parseInt(document.getElementById("qty").value);
+    buyNumber = buyNumber + cartNumber;
+    localStorage.setItem("cartnumber",parseInt(buyNumber));
+    update();
+
+}
+
 function minus(){
     let n = document.getElementById("qty").value;
     if(parseInt(n)>0)document.getElementById("qty").value = parseInt(n)-1;
@@ -6,9 +21,22 @@ function plus(){
     let n = document.getElementById("qty").value;
     document.getElementById("qty").value = parseInt(n)+1;
 }
+
 function start(){
+    let cart = document.getElementById("cart-number");
+    if (localStorage.getItem("cartnumber") !== null) {
+        update();
+        cart.innerHTML = cartNumber;
+    } 
+    else {
+        localStorage.setItem("cartnumber",parseInt(0));
+        update();
+        cart.innerHTML = cartNumber;
+    }
     document.getElementById("minus").addEventListener("click",minus,false);
     document.getElementById("plus").addEventListener("click",plus,false);
+    document.getElementById("addToCart").addEventListener("click",addToCart,false);
 }
 
 window.addEventListener("load",start,false);
+
---
+++ js/menu.js
@@ -0,0 +1,17 @@
+var cartNumber  ;
+
+function start(){
+    let cart = document.getElementById("cart-number");
+    if (localStorage.getItem("cartnumber") !== null) {
+        cartNumber = parseInt(localStorage.getItem("cartnumber"));
+        cart.innerHTML = cartNumber;
+    } 
+    else {
+        console.log("The key does not exist in local storage.");
+        localStorage.setItem("cartnumber",parseInt(0));
+        cartNumber = parseInt(localStorage.getItem("cartnumber"));
+        cart.innerHTML = cartNumber;
+    }
+}
+
+window.addEventListener("load",start,false);
